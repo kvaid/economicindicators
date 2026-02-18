5082388bde70f9e9f0940515cadb21b25134ecdb
Kushagra Vaid <kushagra.vaid@outlook.com>
2026-02-17 20:19:08 -0800

    rolling window set to 3 months for bond yield

--- download_scripts/download_bond_yields.py
+++ download_scripts/download_bond_yields.py
@@ -16,7 +16,7 @@ ERROR_FILE = DATA_DIR / "bond_download_errors.csv"
 
 START_DATE_ETF = "2005-01-01"
 DAILY_RULE = "D"
-ETF_YIELD_WINDOW_DAYS = 30
+ETF_YIELD_WINDOW_DAYS = 91
 ETF_YIELD_SMOOTH_DAYS = 5
 
 ETF_SERIES: dict[str, dict[str, str]] = {
